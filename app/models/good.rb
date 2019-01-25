3e6b0abb285e1608bb46dc0c120edff6a07648d9
Elliott Hébert <ell.heb@container4.ch>
2019-01-25 14:09:19 +0100

    add relations to models

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,2 +1,4 @@
 class Good < ApplicationRecord
+  has_many :price_reports
+  has_many :locations, trhough: :price_reports
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,6 @@
 class Location < ApplicationRecord
   belongs_to :parent, class_name: 'Location', optional: true
   has_many :children, class_name: 'Location', foreign_key: 'parent_id'
+  has_many :price_reports
+  has_many :goods, through: :price_reports
 end
